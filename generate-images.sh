d7d42827e50b4dcbdfe1d39763f35f295524254c
Arthur006 <arthur.hoffmann006@gmail.com>
2025-08-04 14:14:40 +1000

    Add client process page and sequence diagram.

--- generate-images.sh
+++ generate-images.sh
@@ -3,3 +3,4 @@
 python puml-generator.py -o docs/images/context.svg puml/context.puml
 python puml-generator.py -o docs/images/container.svg puml/container.puml
 python puml-generator.py -o docs/images/login-sequence.svg puml/login-sequence.puml
+python puml-generator.py -o docs/images/client-sequence.svg puml/client-sequence.puml
